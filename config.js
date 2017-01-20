859fc432bdf373dcee274132fbb014d592099066
Philipp Schwemberger <philipp.schwemberger@gmx.at>
2017-01-20 11:32:39 +0100

    Adding explosion animation
    When a container of sprites is clicked, an explosion is displayed at the position of the click.
    After having troubles with the PIXI.Movieclip() function for sprite sheet animation I made a small animation consisting of 3 images in the explosion() function.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     
-    search_param:        'Bernie Sanders',
+    search_param:        '#NotMyPresident',
     language_param:      'en'
 
 }
--- linkedScripts/main.js
+++ linkedScripts/main.js
@@ -1,11 +1,34 @@
-//this is the js which is doing all the pixi-work
+//this is the js which is doing all the pixi-work and receiving the tweet-data via socket.io
 
 
-var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight, {backgroundColor: 0x1099bb});
+var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight, {backgroundColor: 0x1099bb, resolution: 1});
 document.body.appendChild(renderer.view);
 
 var stage = new PIXI.Container();
+var allObj = new PIXI.Container();  //container for the diff parts of one tweet
+var trump;
 
+
+
+//load & place trump
+//load explosions
+PIXI.loader.add([   //only one loader function allowed
+    'http://i.imgur.com/MbjpZIK.png', 'http://i.imgur.com/iaJheYx.png', 'http://i.imgur.com/zYWTVSo.png', 'http://i.imgur.com/XyGIMu1.png', 
+]).load(loadFinished); 
+//this loads the image as webgl ready texture to >>PIXI.utils.TextureCache[nameOfFile]<<
+
+function loadFinished(){
+    console.log('pic loaded');
+    trump = new PIXI.Sprite(PIXI.loader.resources['http://i.imgur.com/MbjpZIK.png'].texture);
+    trump.anchor.set(0.5, 1);
+    trump.scale.set(0.5, 0.5);
+    trump.position.set(window.innerWidth/2, window.innerHeight);
+    stage.addChild(trump);
+
+    exp1 = new PIXI.Sprite(PIXI.loader.resources['http://i.imgur.com/iaJheYx.png'].texture);
+    exp2 = new PIXI.Sprite(PIXI.loader.resources['http://i.imgur.com/zYWTVSo.png'].texture);
+    exp3 = new PIXI.Sprite(PIXI.loader.resources['http://i.imgur.com/XyGIMu1.png'].texture);
+}
 
 //start gameLoop()  - gets called every 60 seconds
 gameLoop();
@@ -30,21 +53,21 @@ socket.on('stream', function(tweet){
         var obj = new PIXI.Container();
 
         var rectWidth = 500;
-        var rectHeight = 100;
+        var rectHeight = 115;
 
         //create random positions for tweet obj
-        var pos = [getRandomInt(0, window.innerWidth - rectWidth), getRandomInt(0, window.innerHeight - rectHeight)];
+        var pos = [getRandomInt(0, window.innerWidth - rectWidth), getRandomInt(0, window.innerHeight - rectHeight - trump.height)];
 
         var rectangle = new PIXI.Graphics();
-            rectangle.lineStyle(1, 0xFFFFFF, 1);
-            rectangle.beginFill(0x66CCFF);
-            rectangle.drawRoundedRect(0, 0, rectWidth, rectHeight, 8);
+            rectangle.lineStyle(1, 0x000000, 0.5);
+            rectangle.beginFill(0xFFFFFF);
+            rectangle.drawRect(0, 0, rectWidth, rectHeight);
             rectangle.endFill();
             rectangle.position.set(pos[0], pos[1]);
         
     
         
-        //text processing
+        //TWEET text processing
         var fontSzUser = 16;
         var fontSzTweet = 14;
         var fontSzTime = 12;
@@ -59,15 +82,19 @@ socket.on('stream', function(tweet){
         createdAt = 'created at: ' + tweet.created_at.substring(11,19)+' GMT';
 
 
-        var username = new PIXI.Text(tweetUser, {fontFamily: "Arial", fontSize: fontSzUser, fill: 'white'});
+        var username = new PIXI.Text(tweetUser, {fontFamily: "Arial", fontSize: fontSzUser, fill: '#000000'});
+        username.resolution = 2;    //don't know it that really helps
         username.position.set(pos[0] + xOff, pos[1] + yOff);
 
-        var time = new PIXI.Text(createdAt, {fontFamily: "Arial", fontSize: fontSzTime, fill: 'white'});
+        var time = new PIXI.Text(createdAt, {fontFamily: "Arial", fontSize: fontSzTime, fill: '#000000'});
+        time.resolution = 2;
         time.position.set(pos[0] + xOff, pos[1] + yOff + fontSzUser*1.15);
 
-        var message = new PIXI.Text(tweetText, {fontFamily: "Arial", fontSize: fontSzTweet, fill: 'white', wordWrap: true, wordWrapWidth: (rectWidth-xOff*2)}); //fill: '#55ACEE' 
+        var message = new PIXI.Text(tweetText, {fontFamily: "Arial", fontSize: fontSzTweet, fill: '#000000', wordWrap: true, wordWrapWidth: (rectWidth-xOff*2)}); //fill: '#55ACEE' 
         message.position.set(pos[0] + xOff, pos[1] + yOfftweet);
      
+
+    
 
         obj.addChild(rectangle);
         obj.addChild(username);
@@ -78,19 +105,23 @@ socket.on('stream', function(tweet){
         obj.interactive = true;
         obj.on('mousedown', onMouseDown);
        
-        function onMouseDown(){
+        function onMouseDown(event){    //mouseEvent
+            var x = event.data.getLocalPosition(obj).x;     //get the mouseX,Y where the user clicked
+            var y = event.data.getLocalPosition(obj).y;
             //remove obj when clicked on it
-            stage.removeChild(this);
+            allObj.removeChild(this);
             console.log('object removed');
+            explode(x, y);  //call explosion function and place it at the last click on an interactive sprite
         }
         
-        stage.addChild(obj);
-        console.log('objects in stage: ' + stage.children.length);
+        allObj.addChild(obj);
+        stage.addChild(allObj);
+        console.log('objects in stage: ' + allObj.children.length);
 
         
         //if there are more than 5 tweets delete the first one
-        if(stage.children.length > 3){
-            stage.removeChild(stage.children[0]);
+        if(allObj.children.length > 6){
+            allObj.removeChild(allObj.children[0]);
         }
 
 
@@ -99,13 +130,81 @@ socket.on('stream', function(tweet){
 }); //end of event 'stream'
 
 
-function gameLoop(){
-    requestAnimationFrame(gameLoop);
-
-    renderer.render(stage);
-}
-
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//WORKAROUND because I had troubles with PIXI.MovieClie or PIXI.animatedSprite for making sprite sheet animations 
+function explode(x, y){
+    exp1 = new PIXI.Sprite(PIXI.loader.resources['http://i.imgur.com/iaJheYx.png'].texture);
+     exp1.anchor.set(0.5, 0.7);
+     exp1.position.set(x, y);
+    exp2 = new PIXI.Sprite(PIXI.loader.resources['http://i.imgur.com/zYWTVSo.png'].texture);
+     exp2.anchor.set(0.5, 0.7);
+     exp2.position.set(x, y);
+    exp3 = new PIXI.Sprite(PIXI.loader.resources['http://i.imgur.com/XyGIMu1.png'].texture);
+     exp3.anchor.set(0.5, 0.7);
+     exp3.position.set(x, y);
+     console.log(x, + 'y: ' + y);
+
+     var time = 400;
+
+    stage.addChild(exp1);
+    setTimeout(function(){
+        stage.removeChild(exp1);
+        stage.addChild(exp2);
+        setTimeout(function(){
+            stage.removeChild(exp2);
+            stage.addChild(exp3);
+            setTimeout(function(){
+                stage.removeChild(exp3);
+                //stage.addChild(exp n);
+            }, time);
+        }, time);
+    }, time);
+}
+
+
+//variable Text on screen
+var style = {
+    fontFamily : 'Arial',
+    fontSize : '48px',
+    fontStyle : 'italic',
+    fontWeight : 'bold',
+    fill : '#F7EDCA',
+    stroke : '#4a1850',
+    strokeThickness : 5,
+    dropShadow : true,
+    dropShadowColor : '#000000',
+    dropShadowAngle : Math.PI / 6,
+    dropShadowDistance : 6,
+    wordWrap : true,
+    wordWrapWidth : 1000
+};
+        
+var gameText = new PIXI.Text('Play the most overrated game on the web!', style);
+        gameText.anchor.set(0.5, 0.5);
+        gameText.position.set(window.innerWidth/2, window.innerHeight/2-100);
+
+var hashtag = new PIXI.Text('#NotMyPresident', {fontFamily: "Arial", fontSize: 48, fontStyle : 'italic', fontWeight : 'bold',fill: '#FF0000'});
+        hashtag.anchor.set(0.5, 0.5);
+        hashtag.position.set(window.innerWidth/2, window.innerHeight/2);
+
+    stage.addChild(gameText);
+    stage.addChild(hashtag);
+
+var frameCount = 0;
+
+function gameLoop(){
+    requestAnimationFrame(gameLoop);
+
+    if(frameCount>500){
+        gameText.visible = false;
+        hashtag.visible = false;
+    }
+    frameCount++;
+
+
+    renderer.render(stage);
 }
